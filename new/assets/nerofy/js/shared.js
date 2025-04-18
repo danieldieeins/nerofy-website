bbd43844a6d1c0d1fbe65ce28577478c5d8b17b6
nerotvlive <daniel.niesmann@outlook.com>
2025-04-19 00:00:01 +0200

    Remove automatic javascript redirect to www.nerofy.de

--- new/assets/nerofy/js/shared.js
+++ new/assets/nerofy/js/shared.js
@@ -1,7 +1,3 @@
 if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io/nerofy-website/")) {
     location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io/nerofy-website/","nerofy.de");
-} else {
-    if(location.href.includes("nerofy.de")&&!location.href.includes("www.nerofy.de")) {
-        location.href = location.href.replaceAll("nerofy.de", "www.nerofy.de");
-    }
 }
