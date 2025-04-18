2f7279f08d63e8ccb7536c61da45c941624b55ef
nerotvlive <daniel.niesmann@outlook.com>
2025-04-18 21:36:52 +0200

    Automatically redirect GitHub Pages version to Cloudflare Pages

--- assets/javascript/shared.js
+++ assets/javascript/shared.js
@@ -62,6 +62,6 @@ addEventListener('dragstart', function(e){
     e.preventDefault();
 });
 
-if(location.href.includes("nrfy.net")) {
-    location.href = location.href.replaceAll("nrfy.net", "nerofy.de");
+if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io")) {
+    location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io","nerofy.de");
 }
--- new/assets/nerofy/js/shared.js
+++ new/assets/nerofy/js/shared.js
@@ -1,3 +1,3 @@
-if(location.href.includes("nrfy.net")) {
-    location.href = location.href.replaceAll("nrfy.net", "nerofy.de");
+if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io")) {
+    location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io","nerofy.de");
 }
