2dd8a2ed0b6670ebcd0bb026c1562fae3319b6a6
nerotvlive <daniel.niesmann@outlook.com>
2025-04-18 21:38:25 +0200

    Automatically force www. prefix

--- assets/javascript/shared.js
+++ assets/javascript/shared.js
@@ -64,4 +64,8 @@ addEventListener('dragstart', function(e){
 
 if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io")) {
     location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io","nerofy.de");
+} else {
+    if(location.href.includes("nerofy.de")&&!location.href.includes("www.nerofy.de")) {
+        location.href = location.href.replaceAll("nerofy.de", "www.nerofy.de");
+    }
 }
--- new/assets/nerofy/js/shared.js
+++ new/assets/nerofy/js/shared.js
@@ -1,3 +1,7 @@
 if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io")) {
     location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io","nerofy.de");
+} else {
+    if(location.href.includes("nerofy.de")&&!location.href.includes("www.nerofy.de")) {
+        location.href = location.href.replaceAll("nerofy.de", "www.nerofy.de");
+    }
 }
