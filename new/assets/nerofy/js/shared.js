f0a0804b29891a36fc2ce08233e58df082160d4e
nerotvlive <daniel.niesmann@outlook.com>
2025-04-19 00:21:26 +0200

    Remove nrfy.net redirect

--- new/assets/nerofy/js/shared.js
+++ new/assets/nerofy/js/shared.js
@@ -1,3 +1,3 @@
-if(location.href.includes("nrfy.net")||location.href.includes("danieldieeins.github.io/nerofy-website/")) {
+if(location.href.includes("danieldieeins.github.io/nerofy-website/")) {
     location.href = location.href.replaceAll("nrfy.net", "nerofy.de").replaceAll("danieldieeins.github.io/nerofy-website/","nerofy.de");
 }
