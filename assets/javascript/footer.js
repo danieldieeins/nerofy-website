21c92b4799a30d9d8b74d29a54b50294f8229a9c
nerotvlive <daniel.niesmann@outlook.com>
2025-06-23 20:15:32 +0200

    Add footer.js

---
+++ assets/javascript/footer.js
@@ -0,0 +1,29 @@
+addEventListener("DOMContentLoaded", () => {
+    initFooter();
+});
+
+function initFooter() {
+    let footer = document.getElementById("footer-en");
+    if(footer) {
+        styleFooter(footer);
+        footer.innerHTML += "<a href='https://nerofy.de/rechtliches.html' target='_blank'>Imprint and privacy declarion</a><br>©copyright <a href='https://nerofy.de'>Nerofy Network</a>. All rights reserved."
+    }
+    footer = document.getElementById("footer-de");
+    if(footer) {
+        styleFooter(footer);
+        footer.innerHTML += "<a href='https://nerofy.de/rechtliches.html' target='_blank'>Impressum und Datenschutzerklärung</a><br>©copyright <a href='https://nerofy.de'>Nerofy Netzwerk</a>. Alle Rechte vorbehalten."
+    }
+}
+
+function styleFooter(footer) {
+    footer.className = "footer2"
+    footer.style.position = "absolute";
+    footer.style.top = "100%";
+    footer.style.width = "100%";
+    footer.style.backgroundColor = "#000000";
+    footer.style.color = "#ffffff";
+    footer.style.textAlign = "center";
+    footer.style.userSelect = "none";
+    footer.style.padding = "2rem 0"
+    footer.innerHTML = "<style>.footer2 { color: #ffffff90 !important; a { color: white; text-decoration: none;} a:hover { text-decoration: underline; color: dodgerblue; }}</style>";
+}
